293cf86d10999111dfb54fa59cf3a4bd35c7a8c6
Mayke Santos <maykedesenvolvimento@gmail.com>
2020-11-06 22:14:42 -0300

    test

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3030/',
+  baseURL: 'https://grades-api-md.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
